57fd5fc3d9f5ff7af7e7067b6b331d90e61ecd25
silvercho <tjddms01212@gmail.com>
2023-11-30 14:17:39 +0900

    dev,test yml

--- src/test/java/com/example/sb231130/Sb231130ApplicationTests.java
+++ src/test/java/com/example/sb231130/Sb231130ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.sb231130;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb231130ApplicationTests {
 
     @Test
