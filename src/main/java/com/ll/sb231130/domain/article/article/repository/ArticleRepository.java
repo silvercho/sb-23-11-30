e29aff7fb3182631fb61f1830e0460f5ba9c63aa
silvercho <tjddms01212@gmail.com>
2023-11-30 16:02:37 +0900

    dto2

--- src/main/java/com/ll/sb231130/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231130/domain/article/article/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package com.ll.sb231130.domain.article.article.repository;
 import com.ll.sb231130.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByOrderByIdDesc();
 }
