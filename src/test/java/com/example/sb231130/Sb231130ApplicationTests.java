e800be3d50693bcd1a89df6501c5cbf62a889fa1
silvercho <tjddms01212@gmail.com>
2023-11-30 14:14:44 +0900

    settings

---
+++ src/test/java/com/example/sb231130/Sb231130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb231130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231130ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
