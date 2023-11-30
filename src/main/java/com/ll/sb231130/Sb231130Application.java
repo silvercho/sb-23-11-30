7aa67a931cc1f4977f7eb42a7a7cebf7bfb0768e
silvercho <tjddms01212@gmail.com>
2023-11-30 14:41:10 +0900

    NotProd 작동

--- src/main/java/com/ll/sb231130/Sb231130Application.java
+++ src/main/java/com/ll/sb231130/Sb231130Application.java
@@ -2,8 +2,10 @@ package com.ll.sb231130;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Sb231130Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ll/sb231130/domain/member/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.ll.sb231130.domain.member.member.entity;
+
+import com.ll.sb231130.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@SuperBuilder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Setter
+@Getter
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+}
