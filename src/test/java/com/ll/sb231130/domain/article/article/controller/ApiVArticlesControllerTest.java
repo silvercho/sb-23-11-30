91413f0659a4f7d704db1b806abba1c903d9f2ca
silvercho <tjddms01212@gmail.com>
2023-12-06 10:51:51 +0900

    t3

--- src/test/java/com/ll/sb231130/domain/article/article/controller/ApiVArticlesControllerTest.java
+++ src/test/java/com/ll/sb231130/domain/article/article/controller/ApiVArticlesControllerTest.java
@@ -1,6 +1,8 @@
 package com.ll.sb231130.domain.article.article.controller;
 
 
+import com.ll.sb231130.domain.article.article.entity.Article;
+import com.ll.sb231130.domain.article.article.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static  org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,6 +27,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ApiVArticlesControllerTest {
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private ArticleService articleService;
 
     // 날짜 패턴 정규식
     private static final String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.?\\d{0,7}";
@@ -67,5 +73,29 @@ public class ApiVArticlesControllerTest {
                 .andExpect(jsonPath("$.data.item.authorName", notNullValue()))
                 .andExpect(jsonPath("$.data.item.title", notNullValue()))
                 .andExpect(jsonPath("$.data.item.body", notNullValue()));
+    }
+    @Test
+    @DisplayName("DELETE /api/v1/articles/1")
+    void t3() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(delete("/api/v1/articles/1"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1ArticlesController.class))
+                .andExpect(handler().methodName("removeArticle"))
+                .andExpect(jsonPath("$.data.item.id", instanceOf(Number.class)))
+                .andExpect(jsonPath("$.data.item.createDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.item.modifyDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.item.authorId", instanceOf(Number.class)))
+                .andExpect(jsonPath("$.data.item.authorName", notNullValue()))
+                .andExpect(jsonPath("$.data.item.title", notNullValue()))
+                .andExpect(jsonPath("$.data.item.body", notNullValue()));
+
+        Article article1 = articleService.findById(1L).orElse(null);
+        assertThat(article1).isNull();
     }
 }
